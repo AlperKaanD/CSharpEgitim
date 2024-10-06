2109bbd1ba72c6a2204fc89ac8a4a29895b42a3f
Alper Kaan Dinc <154609899+AlperKaanD@users.noreply.github.com>
2024-10-06 19:30:09 +0300

    CSharp_Egitim_Kampi
    Murat Yücedağ Youtube C# Eğitim Kampı

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello World");
+             Console.Write("Selam");*/
+            #region StringDegiskenler
+
+            //String name;
+            //name = "Alper";
+
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, distirict, city;
+
+            //customerName = "Ali";
+            //customerSurname = "Cinar";
+            //customerPhone = "+90543 322 55 33";
+            //customerEmail = "aaaa@gmail.com";
+            //distirict = "Alanya";
+            //city = "Antalya";
+
+            //Console.WriteLine("***** Rezervasyon Karti*****");
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------------------");
+            //Console.WriteLine("Musteri:" + customerName + " " + customerSurname);
+            //Console.WriteLine("Iletisim:" + customerPhone);
+            //Console.WriteLine("Email Adresi:" + customerEmail);
+            //Console.WriteLine("Adres:"+distirict+"/"+city);
+            //Console.WriteLine("------------------------------------------");
+
+            //customerName = "Aysegul";
+            //customerSurname = "Kaya";
+            //customerPhone = "+90543 322 80 90";
+            //customerEmail = "bbb@gmail.com";
+            //distirict = "Adapazari";
+            //city = "Sakarya";
+
+            //Console.WriteLine("***** Rezervasyon Karti*****");
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------------------");
+            //Console.WriteLine("Musteri:" + customerName + " " + customerSurname);
+            //Console.WriteLine("Iletisim:" + customerPhone);
+            //Console.WriteLine("Email Adresi:" + customerEmail);
+            //Console.WriteLine("Adres:"+distirict+"/"+city);
+            //Console.WriteLine("------------------------------------------");
+
+
+            #endregion
+
+          
+
+            Console.Read();
+
+            
+        }
+    }
+}
